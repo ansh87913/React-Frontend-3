6da02146385687012ac7f776e31cfe6e0dee7302
Ansh Jain <ansh.jain2021@vitstudent.ac.in>
2023-10-15 13:36:03 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { Task } from "./components/Task_3";
+import { Task } from "./components/Tasks/Task_3";
 
 function App() {
   const arr = ["red", "green", "blue", "yellow", "pink", "skyblue", "yellow2", "purple", "pink2", "green2", "pink3", "blue2", "brown", "orange", "blue3", "yellow"];
